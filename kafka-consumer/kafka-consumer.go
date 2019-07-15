02596eeb9d55da6edfcd386f5456ec3aee6ddf31
Sean Murphy <sean@gopaddy.ch>
2019-07-15 18:12:21 +0200

    Changed format to produce deltas based on epoch millisecond formt

--- kafka-consumer/kafka-consumer.go
+++ kafka-consumer/kafka-consumer.go
@@ -63,15 +63,17 @@ func main() {
 		val := m.Value
 		p := ParsedMessage{}
 		json.Unmarshal(val, &p)
-		unixTime := time.Now().Unix()
-		payloadTimestamp := p.Payload.Timestamp
+		unixTimeMillis := time.Now().UnixNano() / 1000
+		payloadTimestamp := int64(p.Payload.Timestamp)
 		createdAt := p.Payload.After.CreatedAt
 
 		formatString := "2006-01-02T15:04:05.000Z"
 		createdAtTime, _ := time.Parse(formatString, createdAt)
 
-		fmt.Printf("Record ID:%v,  Time(current): %v, payload ts_ms: %v, created_at: %v, ID: %v\n",
-			p.Payload.After.ID, unixTime, payloadTimestamp, createdAtTime.Unix())
+		createdAtTimeEpoch := createdAtTime.UnixNano() / 1000
+
+		fmt.Printf("Record ID:%v,  Time(created_at): %v, Delta(payload ts_ms): %v, Delta(current time): %v\n",
+			p.Payload.After.ID, createdAtTimeEpoch, payloadTimestamp-createdAtTimeEpoch, unixTimeMillis-createdAtTimeEpoch)
 	}
 
 	r.Close()
